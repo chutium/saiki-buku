e6927b219c52faf28e05ffc7c36a161b84be269d
Fabian Wollert <darkwolli32@gmail.com>
2015-06-28 22:39:42 +0200

    init

---
+++ download_kafka.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ref. https://github.com/wurstmeister/kafka-docker/blob/master/download-kafka.sh
+
+mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | sed -rn 's/.*"preferred":.*"(.*)"/\1/p')
+url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
