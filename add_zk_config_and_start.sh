d2401f5149349d8c6e328144474d8585273274e2
Fabian Wollert <darkwolli32@gmail.com>
2015-06-28 22:22:26 +0200

    init

---
+++ add_zk_config_and_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "writing config with zk info"
+echo "" >> /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/server.properties
+echo zookeeper.connect=$ZOOKEEPER_CONN_STRING >> /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/server.properties
+echo "" >> /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/server.properties
+echo "writing id to config_file"
+python3 /tmp/find_out_own_id.py -f /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/server.properties
+
+exec /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/bin/kafka-server-start.sh /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/config/server.properties
