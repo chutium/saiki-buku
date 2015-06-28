e001d2f9348bb74c60b8ce2fad3d37b93e887636
Fabian Wollert <darkwolli32@gmail.com>
2015-06-28 22:40:26 +0200

    init

---
+++ find_out_own_id.py
@@ -0,0 +1,28 @@
+import boto3
+from boto3.session import Session
+import requests
+from optparse import OptionParser
+import os
+
+parser = OptionParser()
+parser.add_option("-f", "--file", type="string", dest="file", help="which config file should the host to be added on?")
+
+(options, args) = parser.parse_args()
+
+#mandatory options
+if options.file is None:   # if filename is not given
+    parser.error('file not given')
+    sys.exit(2)
+else:
+    config_file = options.file
+
+url = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
+response = requests.get(url)
+json = response.json()
+region = json['region']
+instanceId = json['instanceId']
+privateIp = json['privateIp']
+myid = privateIp.rsplit(".",1)[1]
+
+with open(config_file, mode='a', encoding='utf-8') as a_file:
+    a_file.write('broker.id=' + myid)
