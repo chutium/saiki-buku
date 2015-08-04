addf23a1f2dd52754169f957e64fd15a6eb20fdc
Fabian Wollert <fabian.wollert@zalando.de>
2015-08-04 16:45:25 +0200

    merge last commit

--- find_out_own_id.py
+++ find_out_own_id.py
@@ -18,31 +18,17 @@ def get_broker_unique_id(broker_id):
     return broker_id
 
 def run():
-<<<<<<< HEAD
-	config_file = os.getenv('KAFKA_DIR') + '/config/server.properties'
-	url = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
-	try:
-		response = requests.get(url)
-		json = response.json()
-		myid = json['privateIp'].rsplit(".", 1)[1]	
-	except requests.exceptions.ConnectionError:
-	    myid="1"
-
-	with open(config_file, mode='a', encoding='utf-8') as a_file:
-	    a_file.write('broker.id=' + myid)
-
-	return myid
-=======
     config_file = os.getenv('KAFKA_DIR') + '/config/server.properties'
     url = 'http://169.254.169.254/latest/dynamic/instance-identity/document'
-    response = requests.get(url)
-    json = response.json()
-    region = json['region']
-    instanceId = json['instanceId']
-    privateIp = json['privateIp']
-    myid = privateIp.rsplit(".", 1)[1]
-    broker_unique_id = get_broker_unique_id(myid)
+    try:
+        response = requests.get(url)
+        json = response.json()
+        myid = json['privateIp'].rsplit(".", 1)[1]
+    except requests.exceptions.ConnectionError:
+        myid="1"
 
+    broker_unique_id = get_broker_unique_id(myid)
     with open(config_file, mode='a', encoding='utf-8') as a_file:
         a_file.write('broker.id=' + broker_unique_id)
->>>>>>> d9cffe4a90ea486fc233e43abc4821efb3c786c2
+
+    return broker_unique_id
